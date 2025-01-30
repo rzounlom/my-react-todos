c1cfc05e36f893b344b424e0fbbdd541f1fc751d
rzounlom <rzounlom@gmail.com>
2025-01-30 18:50:50 -0500

    add state; delete/toggle/create todo

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,48 @@ import "./App.css";
 import { Button } from "react-bootstrap";
 import TodoList from "./components/TodoList";
 import { defaultTodos } from "./data";
+import { useState } from "react";
 
 function App() {
   console.log(defaultTodos);
+  const [todos, setTodos] = useState(defaultTodos); //State to store todos
+
+  const addTodo = () => {
+    const newTodo = {
+      id: todos.length + 1,
+      title: "New Todo",
+      completed: false,
+    };
+    setTodos([...todos, newTodo]);
+  };
+
+  const toggleComplete = (id) => {
+    const updatedTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        todo.completed = !todo.completed;
+      }
+      return todo;
+    });
+
+    setTodos(updatedTodos);
+  };
+
+  const deleteTodo = (id) => {
+    const updatedTodos = todos.filter((todo) => todo.id !== id);
+    setTodos(updatedTodos);
+  };
 
   return (
     <div className="container">
       <h1>Todo App Example</h1>
-      <Button className="mt-2 mb-2">Add Todo</Button>
-      <TodoList todos={defaultTodos} />
+      <Button className="mt-2 mb-2" onClick={addTodo}>
+        Add Todo
+      </Button>
+      <TodoList
+        todos={todos}
+        toggleComplete={toggleComplete}
+        deleteTodo={deleteTodo}
+      />
     </div>
   );
 }
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -2,17 +2,22 @@ import "./Todo.css";
 
 import { Button } from "react-bootstrap";
 
-export default function Todo({ todo }) {
+export default function Todo({ todo, toggleComplete, deleteTodo }) {
   return (
     <div className="todo">
       <p className={todo.completed ? "todo-title-completed " : ""}>
         {todo?.title}
       </p>
       <div className="btn-group">
-        <Button variant="outline-primary">
+        <Button
+          variant="outline-primary"
+          onClick={() => toggleComplete(todo.id)}
+        >
           {todo.completed ? "Undo" : "Complete"}
         </Button>
-        <Button variant="outline-danger">Delete</Button>
+        <Button variant="outline-danger" onClick={() => deleteTodo(todo.id)}>
+          Delete
+        </Button>
       </div>
     </div>
   );
